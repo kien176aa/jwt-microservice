780dfef00b233b5a7d50a3b6a9fb15256c0c8ee8
DatLaid <datlaid@gmail.com>
2025-06-13 16:09:30 +0700

    fix docker

--- sql/url_access.sql
+++ sql/url_access.sql
@@ -1,4 +1,7 @@
 -- Insert URL access data only if role doesn't exist
+
+USE javatechie;
+
 INSERT INTO url_access (role, url)
 SELECT 'ALLOW_URL', '/auth,/ws'
     WHERE NOT EXISTS (SELECT 1 FROM url_access WHERE role = 'ALLOW_URL');
